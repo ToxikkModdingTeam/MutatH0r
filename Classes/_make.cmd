3fa469db0646753f96c956137480ae9f3e3b763d
hbeham <horst@beham.biz>
2016-05-25 22:06:38 +0200

    cleaning up

--- Classes/_make.cmd
+++ Classes/_make.cmd
@@ -1,20 +1,23 @@
 @echo off
 cd /d "%~dp0"
 set cwd=%cd%
+cd ..\..\..\..
+set TOXIKK=%cd%
+cd "%cwd%"
 
 if "%1"=="compress" goto compress
 
 :build
-copy MutatH0r_Content.upk ..\..\..\..\UDKGame\Content >nul
+copy MutatH0r_Content.upk %TOXIKK%\UDKGame\Content >nul
 if errorlevel 1 goto error
-F:\games\Steam\SteamApps\common\TOXIKK\Binaries\Win32\TOXIKK.exe make -configsubdir=LocalBuild
+%TOXIKK%\Binaries\Win32\TOXIKK.exe make -configsubdir=LocalBuild
 if errorlevel 1 goto error
 goto :eof
 
 :compress
-cd ..\..\..\..\UDKGame\Script_LocalBuild
+cd %TOXIKK%\UDKGame\Script_LocalBuild
 if errorlevel 1 goto error
-F:\games\Steam\SteamApps\common\TOXIKK\Binaries\Win32\TOXIKK.exe stripsource MutatH0r.u -configsubdir=LocalBuild
+%TOXIKK%\Binaries\Win32\TOXIKK.exe stripsource MutatH0r.u -configsubdir=LocalBuild
 if errorlevel 1 goto error
 goto :eof
 
--- Classes/_publish.cmd
+++ Classes/_publish.cmd
@@ -10,7 +10,7 @@ if errorlevel 1 set error=1
 
 copy d:\sources\ToxikkServerLauncher\ToxikkServerLauncher\MyServerConfig.ini d:\sources\ToxikkServerLauncher\ToxikkServerLauncher\bin\Debug\MyServerConfig.ini 2>nul
 
-for %%d in (f:\games\Steam\SteamApps\common\TOXIKK\UDKGame f:\games\Steam\SteamApps\common\TOXIKK\WorkshopUploader\MutatH0r\UploadContent c:\steamcmd\steamapps\workshop\content\324810\MutatH0r) do (
+for %%d in (f:\games\Steam\SteamApps\common\TOXIKK\UDKGame c:\steamcmd\steamapps\workshop\content\324810\MutatH0r f:\games\Steam\SteamApps\common\TOXIKK\WorkshopUploader\MutatH0r\UploadContent) do (
   for %%e in (Script Content Config) do (
     if not exist %%d\%%e mkdir %%d\%%e
     if errorlevel 1 set error=1
@@ -18,16 +18,17 @@ for %%d in (f:\games\Steam\SteamApps\common\TOXIKK\UDKGame f:\games\Steam\SteamA
   
   copy MutatH0r.u %%d\Script >nul
   if errorlevel 1 set error=1
-  copy PredatH0r.u %%d\Script >nul
-  if errorlevel 1 set error=1
-  
   copy MutatH0r_Content.upk %%d\Content >nul
-  if errorlevel 1 set error=1
-  
+  if errorlevel 1 set error=1 
   copy UDKMutatH0r.ini %%d\Config >nul
   if errorlevel 1 set error=1
-  copy UDKPredatH0r.ini %%d\Config >nul
-  if errorlevel 1 set error=1
+  
+  if not "%%d" == "f:\games\Steam\SteamApps\common\TOXIKK\WorkshopUploader\MutatH0r\UploadContent" (
+    copy PredatH0r.u %%d\Script >nul
+    if errorlevel 1 set error=1 
+    copy UDKPredatH0r.ini %%d\Config >nul
+    if errorlevel 1 set error=1
+  )
 )
 if %error%==0 goto :eof
 pause
